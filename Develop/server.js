f6125dd796f1d8735b2de02f36aa6eeb534009d5
Jordan Scott <jscott0751@gmail.com>
2021-09-16 21:23:54 -0400

    completed notetaker

--- Develop/server.js
+++ Develop/server.js
@@ -2,27 +2,54 @@ const express = require("express");
 const path = require('path');
 const fs = require('fs');
 
-const PORT = process.env.PORT || 3001  
+const PORT = process.env.PORT || 3001
 const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/api/notes', function(req, res) {
-// logic 
-
+// get request
+app.get('/api/notes', function (req, res) {
+    // logic 
+    fs.readFile('./db/db.json', 'utf8', function (err, data) {
+        if (err) {
+            res.status(500).json(err)
+        }
+        res.json(JSON.parse(data))
+    })
 })
 
-app.post(`/api/notes/`, function(req, res) {
-// logic 
+// post request
+app.post(`/api/notes/`, function (req, res) {
+    // logic 
 
-})
+    console.log('body', req.body)
+    fs.readFile('./db/db.json', 'utf8', function (err, data) {
+        if (err) {
+            res.status(500).json(err)
+        }
 
-app.delete(`/api/notes/:id`, function (req, res) {
-// logic 
+        const noteData = JSON.parse(data)
 
-})
+        noteData.push({ title: req.body.title, text: req.body.text, id: '1' })
+
+        fs.writeFile('./db/db.json', JSON.stringify(noteData), function(err) {
+            if (err) {
+                res.status(500).json(err)
+            }
+        })
+
+        res.json(JSON.parse(data))
+    })
+});
+
+
+// //delete request
+// app.delete(`/api/notes/:id`, function (req, res) {
+//     // logic 
+
+// })
 
 
 
@@ -39,6 +66,6 @@ app.get('/notes', (req, res) =>
 
 
 // server creation
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log('server running')
 });
